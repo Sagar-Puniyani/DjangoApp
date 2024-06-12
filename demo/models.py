3bce2c767592b3c5b83d93c6518bcc0ead0408b9
Sagar <sagarpuniyani@gmail.com>
2024-06-12 21:34:14 +0530

    Add models

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Varitey(models):
+    VARITRY_OF_CHOICE = [
+        ('DL','DEEP_LEARNING'),
+        ('ML','MACHINE_LEARNING')
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='Images/')
+    date = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2,choices=VARITRY_OF_CHOICE)
